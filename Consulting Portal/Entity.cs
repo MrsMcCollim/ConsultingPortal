7680b60771d2c3d36673fc94aed0ae4ee71e13a9
Sarah McCollim <sarahjmccollim@gmail.com>
2020-04-18 18:18:51 -0600

    Week 2 Changes

--- Consulting Portal/Entity.cs
+++ Consulting Portal/Entity.cs
@@ -5,15 +5,35 @@ using System.Text;
 
 namespace Consulting_Portal
 {
+    #region Enums
+    /// <summary>
+    /// Provides values for Entity Type array
+    /// </summary>
+    enum EntityType
+    {
+        PublicSchoolDistrict,
+        Township,
+        IVConsultant,
+        IVUser,
+        }
+    #endregion
+
+    #region Entity
     /// <summary>
     /// Entity = IC, IM, District User, District
     /// </summary>
     class Entity
-    {       
+    {
         /// <summary>
-        /// Unique Identifier for Entity Record
+        /// Last Entity Identifier = Last Entity Indentifier to be assigned to an Entity upon the creation of a new Entity Record
         /// </summary>
-        public int EntityIdentifier { get; set; }
+        private static int LastEntityIdentifier = 0;
+        private static object entities;
+
+        /// <summary>
+        /// Unique Identifier for an Entity Record, Autopopulated, Sequential Assignment
+        /// </summary>
+        public int EntityIdentifier { get; private set; }
 
         /// <summary>
         /// Entity Name = Legal Name
@@ -23,22 +43,69 @@ namespace Consulting_Portal
         /// <summary>
         /// Entity Type = Entity / Person
         /// </summary>
-        public string EntityType { get; set; }
+        public EntityType EntityType { get; set; }
+
         /// <summary>
         /// Creation Date = Creation Date of Entity Record
         /// </summary>
-        public DateTime CreationDate { get; set; }
+        public DateTime CreationDate { get; private set; }
+
         /// <summary>
         /// Contact Phone Number = Entity's Contact Phone Number
         /// </summary>
-        public PhoneAttribute ContactPhoneNumber { get; set; }
+        public string ContactPhoneNumber { get; set; }
+
         /// <summary>
         /// Email Address = Entity's Email Address
         /// </summary>
-        public EmailAddressAttribute EmailAddress { get; set; }
+        public string EmailAddress { get; set; }
+
         /// <summary>
         /// Web Address = Entity's Web Address
         /// </summary>
-        public UrlAttribute WebAddress { get; set; }
+        public string WebAddress { get; set; }
+
+        /// <summary>
+        /// Account Balance = Entity's Account Balance in USD
+        /// </summary>
+        public decimal AccountBalance { get; private set; }
+        #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Entity Identifier is assigned upon creation of new Entity Record, Pre-increment assignment
+    /// Creation Date = Time and date stamp of Entity's creation
+    /// </summary>
+    public Entity()
+    {
+       EntityIdentifier = ++LastEntityIdentifier;
+       CreationDate = DateTime.Now;
+    }
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Purchase = Addition to Account Balance
+    /// </summary>
+    /// <param name="amount">Amount of Entity Purchase</param>
+    public decimal Purchase(decimal amount)
+    {
+       AccountBalance += amount;
+       return AccountBalance;
+    }
+        
+    /// <summary>
+    /// Use = Reduction of Account Balance
+    /// </summary>
+    /// <param name="amount">Amount of Entity Use</param>
+    public decimal Use(decimal amount)
+    {
+       AccountBalance -= amount;
+       return AccountBalance;
+    }
+    #endregion
+
     }
 }
---
+++ Consulting Portal/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consulting_Portal
+{
+    class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Description { get; set; }
+        public
+    }
+}
